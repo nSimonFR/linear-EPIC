10ebefa7aa773e925fa16c8bf36367ba388fb2b6
Nicolas Simon <nsimon@pm.me>
2023-12-15 21:07:48 +0100

    Update to force sync of state

--- src/update-epic.ts
+++ src/update-epic.ts
@@ -6,43 +6,38 @@ const hasLabel = async (issue: Issue, labelToCheck: string) => {
   return labels.nodes.some((l) => l.name === labelToCheck);
 };
 
-export const updateParentState = (linearClient: LinearClient) => async (issueId: string, labelToCheck = "EPIC") => {
-  const issue = await linearClient.issue(issueId);
+export const updateParentState = (linearClient: LinearClient) => async (issueId: string, labelToCheck: string) => {
+  let issue = await linearClient.issue(issueId);
 
-  const parent = await issue.parent;
-  if (!parent) {
-    if (!hasLabel(issue, labelToCheck)) {
-      return console.debug(`Stopping - No parent & not ${labelToCheck}`);
-    }
-
-    // TODO allow automatic update from issue
-
+  if (!issue.parent) {
     await linearClient.updateIssue(issueId, { estimate: 0 });
   } else {
-    if (!hasLabel(parent, labelToCheck)) {
-      return console.debug(`Stopping - Parent not a ${labelToCheck}.`);
-    }
-
-    console.info(`Found ${labelToCheck}: ${parent.title}.`);
-
-    const parentTeam = await parent.team;
-    const statesResult = await linearClient.workflowStates({
-      filter: { team: { id: { eq: parentTeam!.id } } },
-    });
-    const states = statesResult.nodes;
-
-    const childrens = await parent.children();
-    const state = await findState(parent, childrens.nodes, states);
-    if (!state) {
-      return console.log(`[State] Stopping - No state.`);
-    }
-
-    const parentState = await parent.state;
-    if (state.id === parentState!.id) {
-      return console.log(`No need to update - matching state ${state.name}.`);
-    }
-    console.info(`Updating ${labelToCheck} to state: ${state.name}.`);
-
-    await linearClient.updateIssue(parent.id, { stateId: state.id });
+    issue = await issue.parent;
   }
+
+  if (!hasLabel(issue, labelToCheck)) {
+    return console.debug(`Stopping - Issue not a ${labelToCheck}.`);
+  }
+
+  console.info(`Found ${labelToCheck}: ${issue.title}.`);
+
+  const parentTeam = await issue.team;
+  const statesResult = await linearClient.workflowStates({
+    filter: { team: { id: { eq: parentTeam!.id } } },
+  });
+  const states = statesResult.nodes;
+
+  const childrens = await issue.children();
+  const state = await findState(issue, childrens.nodes, states);
+  if (!state) {
+    return console.log(`[State] Stopping - No state.`);
+  }
+
+  const parentState = await issue.state;
+  if (state.id === parentState!.id) {
+    return console.log(`No need to update - matching state ${state.name}.`);
+  }
+  console.info(`Updating ${labelToCheck} to state: ${state.name}.`);
+
+  await linearClient.updateIssue(issue.id, { stateId: state.id });
 };
